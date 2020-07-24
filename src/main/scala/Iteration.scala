2f3ce74a12e0b16fd3701ee801ec5b5d1906c779
hungphan <colorvisavn@gmail.com>
2020-07-25 00:06:51 +0700

    Add example for iteration with stream

---
+++ src/main/scala/Iteration.scala
@@ -0,0 +1,25 @@
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.{
+  DataStream,
+  StreamExecutionEnvironment
+}
+
+object Iteration extends App {
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  val data: DataStream[Int] = env.fromElements(0)
+  val iteration = data.iterate((stepFunction: DataStream[Int]) => {
+    val feedback = stepFunction
+      .filter(_ < 10)
+      .map(_ + 1)
+      .setParallelism(1)
+
+    feedback.print()
+
+    val output = stepFunction.filter(_ == 10)
+
+    (feedback, output)
+  })
+
+  env.execute("Iteration")
+}
