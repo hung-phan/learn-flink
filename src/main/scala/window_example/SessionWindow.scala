97f620c926f0608976ddbfd1dc0f9d077492e123
hungphan <colorvisavn@gmail.com>
2020-08-02 07:34:38 +0700

    Add example for session window

---
+++ src/main/scala/window_example/SessionWindow.scala
@@ -0,0 +1,42 @@
+package window_example
+
+import java.sql.Timestamp
+
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+
+object SessionWindow extends App {
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+  val data = env.socketTextStream("localhost", 9090)
+
+  val sum = data
+    .map { input =>
+      val words = input.split(",")
+
+      (words(0).toLong, words(1))
+    }
+    .assignTimestampsAndWatermarks(
+      new AscendingTimestampExtractor[(Long, String)] {
+        override def extractAscendingTimestamp(element: (Long, String)): Long =
+          element._1
+      }
+    )
+    .windowAll(EventTimeSessionWindows.withGap(Time.seconds(5)))
+    .reduce { (curr, prev) =>
+      val value = prev._2.toInt + curr._2.toInt
+      val t = new Timestamp(System.currentTimeMillis())
+
+      (t.getTime, value.toString)
+    }
+
+  sum.writeAsText("tmp/window_data")
+
+  env.execute("Window")
+}
